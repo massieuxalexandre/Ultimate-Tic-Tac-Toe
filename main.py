2ddc7103c5bba38c44036f10382dbeabc1ba5b06
massieuxalexandre <massieuxalexandre@gmail.com>
2026-05-05 13:50:44 +0200

    depth = 7

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     # création de 2 joueurs (1 humain qui sera l'utilisateur et 1 ia)
     human = Player("Humain", 'X')
     # ai = Player("AI", 'O')
-    ai = AI("AI", 'O', 6)
+    ai = AI("AI", 'O', 7)
     
     # création de la grille principale contenant 9 sous grilles (jouée par les 2 joueurs)
     mini_grids_locations = ["top_left", "top_center", "top_right",
